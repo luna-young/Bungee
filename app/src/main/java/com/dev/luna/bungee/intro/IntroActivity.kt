b3d99fdc9464df47679a6f89a1188b728ac68401
Soyoung Luna Yang <luna.ensocorro@gmail.com>
2020-05-12 21:18:29 +0900

    앱 실행 시 Api 호출 테스트

--- app/src/main/java/com/dev/luna/bungee/intro/IntroActivity.kt
+++ app/src/main/java/com/dev/luna/bungee/intro/IntroActivity.kt
@@ -2,12 +2,29 @@ package com.dev.luna.bungee.intro
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import com.dev.luna.bungee.api.BungeeApi
+import kotlinx.coroutines.runBlocking
 import org.jetbrains.anko.setContentView
+import java.lang.Exception
 
 class IntroActivity : Activity() {
+
+    val TAG = IntroActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val ui = IntroActivityUI()
         ui.setContentView(this)
+
+        runBlocking {
+            try {
+                val response = BungeeApi.instance.hello()
+                Log.d(TAG, response.data)
+            } catch (e: Exception) {
+                Log.e(TAG, "Hello Api 호출 오류", e)
+            }
+        }
+
     }
 }
