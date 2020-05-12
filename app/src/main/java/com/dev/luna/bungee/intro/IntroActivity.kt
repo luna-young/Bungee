2484185acd29ea84fbb1d1bfb065a83c9748a536
Soyoung Luna Yang <luna.ensocorro@gmail.com>
2020-05-12 19:57:15 +0900

    IntroActivity 생성 및 UI 세팅

--- app/src/main/java/com/dev/luna/bungee/intro/IntroActivity.kt
+++ app/src/main/java/com/dev/luna/bungee/intro/IntroActivity.kt
@@ -1,7 +1,13 @@
 package com.dev.luna.bungee.intro
 
 import android.app.Activity
+import android.os.Bundle
+import org.jetbrains.anko.setContentView
 
 class IntroActivity : Activity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val ui = IntroActivityUI()
+        ui.setContentView(this)
+    }
 }
---
+++ app/src/main/java/com/dev/luna/bungee/intro/IntroActivityUI.kt
@@ -0,0 +1,22 @@
+package com.dev.luna.bungee.intro
+
+import android.graphics.Typeface
+import android.view.Gravity
+import android.view.View
+import com.dev.luna.bungee.R
+import org.jetbrains.anko.*
+
+class IntroActivityUI : AnkoComponent<IntroActivity> {
+
+    override fun createView(ui: AnkoContext<IntroActivity>): View {
+        return ui.relativeLayout {
+            gravity = Gravity.CENTER
+
+            textView("Bungee!") {
+                textSize = 24f
+                textColorResource = R.color.colorPrimary
+                typeface = Typeface.DEFAULT_BOLD
+            }
+        }
+    }
+}
