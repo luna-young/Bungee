fd1ec74e9fd7bb5494bdb7082181f7ebbfabed22
Soyoung Luna Yang <luna.ensocorro@gmail.com>
2020-06-08 17:56:53 +0900

    상품정보 등록 UI 생성

--- app/src/main/java/com/dev/luna/bungee/product/ProductMainViewModel.kt
+++ app/src/main/java/com/dev/luna/bungee/product/ProductMainViewModel.kt
@@ -8,7 +8,6 @@ import net.codephobia.ankomvvm.lifecycle.BaseViewModel
 class ProductMainViewModel(app: Application) : BaseViewModel(app) {
 
     fun openRegistrationActivity() {
-        //todo 상품 등록 ui가 준비되면 해당 액티비티를 열도록 수정
         startActivity<ProductRegistrationActivity> {
             flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
         }
--- app/src/main/java/com/dev/luna/bungee/product/registration/ProductRegistrationUI.kt
+++ app/src/main/java/com/dev/luna/bungee/product/registration/ProductRegistrationUI.kt
@@ -1,12 +1,22 @@
 package com.dev.luna.bungee.product.registration
 
+import android.graphics.Color
+import android.text.InputType
 import android.view.Gravity
-import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
+import com.bumptech.glide.Glide
 import com.dev.luna.bungee.R
+import com.dev.luna.bungee.api.ApiGenerator
+import net.codephobia.ankomvvm.databinding.bindString
+import net.codephobia.ankomvvm.databinding.bindStringEntries
+import net.codephobia.ankomvvm.databinding.bindUrl
 import org.jetbrains.anko.*
+import org.jetbrains.anko.design.textInputEditText
+import org.jetbrains.anko.design.textInputLayout
 import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.jetbrains.anko.sdk27.coroutines.onItemSelectedListener
+import org.jetbrains.anko.sdk27.coroutines.textChangedListener
 
 class ProductRegistrationUI(
         private val viewModel: ProductRegistrationViewModel
@@ -29,8 +39,110 @@ class ProductRegistrationUI(
                 pickImageView(ui, 2)
                 space().lparams(dip(8))
                 pickImageView(ui, 3)
-
             }
+
+            textView("상품평 및 설명") {
+                topPadding = dip(40)
+                textSize = 16f
+                textColorResource = R.color.colorPrimary
+            }
+
+            textInputLayout {
+                topPadding = dip(20)
+                textInputEditText {
+                    hint = "상품명"
+                    setSingleLine()
+                    bindString(ui.owner, viewModel.productName)
+
+                    //anko coroutines에서 제공하는 함수로 텍스트뷰의 텍스트가 변경되었을 때 동작하는 이벤트 리스너
+                    //내부적으로는 텍스트뷰의 addTextChangedListener()를 호출해주고 있음
+                    //onTextChanged의 언더스코어_는 람다의 파라미터 변수를 사용하지 않을 때 간단히 대체할 수 있는 기호
+                    textChangedListener {
+                        onTextChanged { _, _, _, _ ->
+                            viewModel.checkProductNameLength()
+                        }
+                    }
+                }
+                textView("0/40") {
+                    leftPadding = dip(4)
+                    textSize = 12f
+                    textColorResource = R.color.colorPrimary
+                    bindString(ui.owner, viewModel.productNameLength)
+                }
+            }
+
+
+
+            textInputLayout {
+                topPadding = dip(20)
+                textInputEditText {
+                    hint = "상품 설명"
+                    maxLines = 6 //UI가 늘어날 수 있는 최대 길이
+                    bindString(ui.owner, viewModel.description)
+                    textChangedListener {
+                        onTextChanged { _, _, _, _ ->
+                            viewModel.checkDescriptionLength()
+                        }
+                    }
+                }
+                textView("0/500") {
+                    leftPadding = dip(4)
+                    textSize = 12f
+                    textColorResource = R.color.colorPrimary
+                    bindString(ui.owner, viewModel.productNameLength)
+                }
+            }
+
+            textView("카테고리") {
+                topPadding = dip(40)
+                textSize = 16f
+                textColorResource = R.color.colorPrimary
+            }
+
+            verticalLayout{
+                topPadding = dip(12)
+                bottomPadding = dip(12)
+                backgroundColor = 0xEEEEEEEE.toInt()
+
+                spinner {
+                    //bindStringEntries() 함수는 ARrayAdapter를 이용해 MutableLiveData<List<String>> 타입의 데이터를 스피너에 바인딩해줌
+                    bindStringEntries(ui.owner, viewModel.categories)
+
+                    onItemSelectedListener {
+                        onItemSelected{_,_,position,_ ->
+                            viewModel.onCategorySelect(position)
+                        }
+                    }
+                }
+
+            }.lparams(matchParent) {
+                topMargin = dip(20)
+            }
+
+            textView("판매 가격") {
+                topPadding = dip(40)
+                textSize = 16f
+                textColorResource = R.color.colorPrimary
+            }
+
+            textInputLayout {
+                topPadding = dip(20)
+                textInputEditText{
+                    hint = "ex) 1000"
+                    setSingleLine()
+                    inputType = InputType.TYPE_CLASS_NUMBER
+                    bindString(ui.owner, viewModel.price)
+                }
+            }
+
+            button("상품 등록") {
+                backgroundColorResource = R.color.colorPrimary
+                textColor = Color.WHITE
+                onClick {viewModel.register()}
+            }.lparams(matchParent, wrapContent) {
+                topMargin = dip(40)
+            }
+
         }
     }
 
@@ -43,6 +155,16 @@ class ProductRegistrationUI(
         backgroundColor = 0xFFEEEEEE.toInt()
 
         onClick{viewModel.pickImage(imageNum)}
+
+        bindUrl(ui.owner, viewModel.imageUrls[imageNum]) {
+            it?.let{
+                scaleType = ImageView.ScaleType.CENTER_CROP
+                Glide.with(this)
+                        .load("${ApiGenerator.HOST}$it")
+                        .centerCrop()
+                        .into(this)
+            }
+        }
     }.lparams(dip(60), dip(60))
 
 }
