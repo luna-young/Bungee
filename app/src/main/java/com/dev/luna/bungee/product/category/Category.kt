dd0d9ef4a8958f05963349bd0a39d4f757752bc6
Soyoung Luna Yang <luna.ensocorro@gmail.com>
2020-06-11 18:57:24 +0900

    상품 카테고리 변경

--- app/src/main/java/com/dev/luna/bungee/product/category/Category.kt
+++ app/src/main/java/com/dev/luna/bungee/product/category/Category.kt
@@ -5,15 +5,10 @@ data class Category (
         val name: String)
 
     val categoryList = listOf(
-        Category(0, "여성의류"),
-        Category(1, "남성의류"),
-        Category(2, "아동복"),
-        Category(3, "잡화"),
-        Category(4, "가전제품"),
-        Category(5, "생활용품"),
-        Category(6, "도서"),
-        Category(7, "반려동물용품"),
-        Category(8, "식품"),
-        Category(9, "디지털기기"),
-        Category(10, "문구류")
+        Category(0, "상의"),
+        Category(1, "하의"),
+        Category(2, "아우터"),
+        Category(3, "가방"),
+        Category(5, "신발"),
+        Category(6, "액세서리")
     )
